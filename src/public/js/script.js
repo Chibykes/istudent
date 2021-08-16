8538c0e8cd0430beb70d506f80e99f1e1bb8e243
Chibykes <62060851+Chibykes@users.noreply.github.com>
2021-08-16 20:53:54 +0100

    Add files via upload

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,10 +1,15 @@
-import QrScanner from "./qr.js";
+let resultBox = document.querySelector('#result');
+let output = document.querySelector('#result p');
+
+import QrScanner from "./qr-scanner.min.js";
 QrScanner.WORKER_PATH = '../js/qr-scanner-worker.min.js';
 
 const video = document.getElementById('qr-video');
 const camQrResult = document.getElementById('cam-qr-result');
 
 function setResult(label, result) {
+    output.innerText = result;
+    resultBox.style.opacity = 1;
     console.log(result);
 }
 
@@ -18,3 +23,6 @@ window.scanner = scanner;
 
 video.parentNode.insertBefore(scanner.$canvas, video.nextSibling);
 scanner.$canvas.style.display = 'block';
+
+
+resultBox.onclick = () => resultBox.style.opacity = 0;
